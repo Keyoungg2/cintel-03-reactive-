f0cf083b59e973b50a7f519f5280c5bdf7609727
Keyoungg2 <156037472+Keyoungg2@users.noreply.github.com>
2024-03-18 20:53:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -113,5 +113,15 @@ with ui.navset_card_tab(id="tab"):
                 title="Body mass on Islands",
             )
             return pie_chart
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
 
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+@reactive.calc
+def filtered_data():
+    return penguins
   
